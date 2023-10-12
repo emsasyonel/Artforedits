b700237e84b50ecb93cebf7e35accaad0a3be9e3
Emre Bayram SAAT <emrebayramsaat@gmail.com>
2023-10-12 16:30:05 +0300

    üçüncü gönderme

--- anasayfa.php
+++ anasayfa.php
@@ -141,13 +141,13 @@
         <div id="carouselExampleFade" class="carousel slide carousel-fade mt-5">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="./carousel img/indir (6).jpeg" class="d-block w-100" alt="...">
+      <a href="enyeniler.php"><img src="./carousel img/indir (6).jpeg" class="d-block w-100" alt="..."></a>
+      </div>
+        <div class="carousel-item">
+        <a href="enyeniler.php"><img src="./carousel img/indir (7).jpeg" class="d-block w-100" alt="..."></a>
         </div>
         <div class="carousel-item">
-          <img src="./carousel img/indir (7).jpeg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="./carousel img/indir (8).jpeg" class="d-block w-100" alt="...">
+        <a href="enyeniler.php"><img src="./carousel img/indir (8).jpeg" class="d-block w-100" alt="..."></a>
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
---
+++ enyeniler.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>En Yeniler</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+        <div class="container">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <img style="width: 45px; border:2px solid black;border-radius:50%;" src="./logo/333462182_616888286949714_8037899921914414019_n.jpg" alt="">
+    <a class="navbar-brand" href="#">Artforedits</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Anasayfa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Videolar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Görseller</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- navbar bitiş -->
+
+<div id="carouselExampleInterval" class="carousel slide" data-mdb-ride="carousel" data-mdb-interval="false">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <video controls autoplay loop muted  src="./en yeniler video/0406(3).mp4"></video>
+        </div>
+        <div class="carousel-item" data-mdb-interval="2000">
+            <video controls autoplay loop muted src="./en yeniler video/0718(1).mp4"></video>
+        </div>
+        <div class="carousel-item">
+            <video controls autoplay loop muted src="./en yeniler video/Sequence 01.mp4"></video>
+        </div>
+    </div>
+    <button class="carousel-control-prev" data-mdb-target="#carouselExampleInterval" type="button" data-mdb-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" data-mdb-target="#carouselExampleInterval" type="button" data-mdb-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
